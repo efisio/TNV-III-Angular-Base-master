23d7784d2a10b528ed665b3960013ecdcb4262b1
efisio.sarritzu@gmauil.com <efisio.sarritzu@gmail.com>
2021-03-08 18:11:25 +0100

    dounghnut chart con i world data

--- Frontend/src/app/charts/world-data-doughnut-chart/world-data-doughnut-chart.component.ts
+++ Frontend/src/app/charts/world-data-doughnut-chart/world-data-doughnut-chart.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ChartType } from 'chart.js';
-import { Label, MultiDataSet } from 'ng2-charts';
+import { Label, SingleDataSet } from 'ng2-charts';
+import { ApiDailyData } from 'src/app/models/apiDaily.model';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
 
 @Component({
   selector: 'app-world-data-doughnut-chart',
@@ -9,19 +12,81 @@ import { Label, MultiDataSet } from 'ng2-charts';
 })
 export class WorldDataDoughnutChartComponent implements OnInit {
 
-  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-  public doughnutChartData: MultiDataSet = [
-    [350, 450, 100],
-    [50, 150, 120],
-    [250, 130, 70],
+  @Input() worldDailyData: ApiDailyData;
+
+  totalCount : number;
+
+  doughnutChartOptions: any = {
+    responsive: true,
+    cutoutPercentage: 30,
+    aspectRatio: 1,
+    layout: {
+      padding: {
+        left: 20,
+        right: 20,
+        top: 0,
+        bottom: 20,
+      }
+    },
+    legend: {
+      display: true,
+    },
+    title: {
+      display: false,
+    },
+    tooltips: {
+      enabled: true
+    },
+    plugins: {
+      datalabels: {
+        color: '#323d48',
+        formatter: (value, ctx) => {
+          var perc = ((value * 100) / this.totalCount).toFixed(0) + "%";
+          return perc;
+        },
+      },
+    },
+  }
+
+  colors = [
+    { //colore barre
+      backgroundColor: [
+        'rgba(60, 125, 197, 1)',
+        'rgba(100, 208, 113, 1)',
+        'rgba(255, 145, 145, 1)',
+      ],
+      //colore barre su hover
+      hoverBackgroundColor: [
+        'rgba(60, 125, 197, 0.8)',
+        'rgba(100, 208, 113, 0.8)',
+        'rgba(255, 145, 145, 0.8)',
+      ]
+    },
   ];
-  public doughnutChartType: ChartType = 'doughnut';
+
+  doughnutChartPlugins = [ChartDataLabels];
+
+  doughnutChartLabels: Label[] = ['Confermati', 'Guariti', 'Decessi'];
+
+  doughnutChartData: SingleDataSet;
+
+  doughnutChartType: ChartType = 'doughnut';
 
   constructor() { }
 
   ngOnInit() {
-  }
 
-  //TODO integrazione dati
+    this.totalCount = this.worldDailyData.new_confirmed +
+      this.worldDailyData.new_recovered +
+      this.worldDailyData.new_deaths;
+
+    this.doughnutChartData = [
+      this.worldDailyData.new_confirmed,
+      this.worldDailyData.new_recovered,
+      this.worldDailyData.new_deaths
+    ];
+    
+
+  }
 
 }
